7e9b3d92f0138bad1bf17ec355319f04ac4a1bef
von-court <matthias.vongerichten@gmail.com>
2023-10-01 20:41:48 +0200

    edit!

--- src/routes/Reddit.svelte
+++ src/routes/Reddit.svelte
@@ -1,10 +1,10 @@
 <script>
     import { onMount } from 'svelte';
     let posts = [];
-    let subredditName = 'all';
+    let subredditName = 'berlin';
 
     onMount(async () => {
-        const response = await fetch(`https://www.reddit.com/r/${subredditName}/top.json?limit=10`);
+        const response = await fetch(`https://www.reddit.com/r/${subredditName}/hot.json?limit=10`);
         const data = await response.json();
         posts = data.data.children.filter(post => !post.data.stickied).map(post => post.data).slice(0, 6);
     });
