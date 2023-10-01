e58a5dfc72489131db104182be68d03934106b6a
von-court <matthias.vongerichten@gmail.com>
2023-10-01 21:29:08 +0200

    edit!

--- src/routes/Reddit.svelte
+++ src/routes/Reddit.svelte
@@ -4,18 +4,26 @@
     let subredditName = 'berlin';
 
     onMount(async () => {
-        const response = await fetch(`https://www.reddit.com/r/${subredditName}/hot.json?limit=10`);
+        const response = await fetch(`https://www.reddit.com/r/${subredditName}/top.json?limit=10&t=day`);
         const data = await response.json();
         posts = data.data.children.filter(post => !post.data.stickied).map(post => post.data).slice(0, 6);
     });
+
+    function truncate(text, length) {
+        return text.length > length ? text.substring(0, length) + "..." : text;
+    }
 </script>
 
-<input bind:value={subredditName} placeholder="Enter subreddit name" />
+<!--input bind:value={subredditName} placeholder="Enter subreddit name" /-->
 
-{#each posts as post}
+{#each posts as post}  
+    {@debug post}
     <div class="post">
+         {#if post.thumbnail && post.thumbnail !== "self" && post.thumbnail !== "default"}
+            <img src={post.thumbnail} alt="Post thumbnail" class="post-thumbnail" />
+        {/if}
         <div class="post-title">{post.title}</div>
-        <div>{post.selftext}</div>
+        <div>{truncate(post.selftext, 100)}</div>
         <div class="post-stats">🔼 {post.ups} | 💬 {post.num_comments}</div>
     </div>
 {/each}
