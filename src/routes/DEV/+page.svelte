501cd2056a8daeb8ebb6ac7feb48726a54ee6453
Matthias Vongerichten <matthias.vongerichten@gmail.com>
2023-10-07 18:55:34 +0200

    add DEV

---
+++ src/routes/DEV/+page.svelte
@@ -0,0 +1,65 @@
+<script>
+    import { onMount } from 'svelte';
+    let posts = [];
+
+    onMount(async () => {
+        const response = await fetch(`https://dev.to/api/articles?top=1&per_page=6`);
+        posts = await response.json();
+    });
+
+    function truncate(text, length) {
+        return text.length > length ? text.substring(0, length) + "..." : text;
+    }
+</script>
+
+<svelte:head>
+	<title>DEV: Top Six Today</title>
+</svelte:head>
+
+<div class="container">
+    {#each posts as post}  
+        <!-- {@debug post} -->
+        <div class="post">
+            <div class="post-content">
+                <h2 class="post-title"><a href="{post.url}" target="_blank">{post.title}</a></h2>
+                <div class="post-text">{truncate(post.description, 100)}</div>
+                <div class="post-stats">🔼 {post.positive_reactions_count} | 💬 {post.comments_count}</div>
+            </div>
+            {#if post.cover_image}
+                <img src={post.cover_image} alt="Post thumbnail" class="post-thumbnail" />
+            {/if}
+        </div>
+    {/each}
+</div>
+
+<style>
+    .container {
+        max-width: 800px;
+        margin: 0 auto;
+    }
+    .post {
+        display: flex;
+        min-height: 5em;
+        padding: 5px;
+    }
+    .post-title {
+        margin-bottom: 10px;
+        color: black;
+    }
+    .post-stats {
+        font-size: 0.9em;
+        color: #888;
+    }
+    .post-content {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+    }
+    .post-text {
+        flex-grow: 1;
+    }
+    .post-thumbnail {
+        margin-left: 10px;
+        max-height: 5rem;
+    }
+</style>
